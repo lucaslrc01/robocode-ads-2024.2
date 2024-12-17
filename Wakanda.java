86b010d3c5d0d82c8b2cf99a77da529a82f5d967
lucas.gd15 <lucas.gd15@aluno.ifsc.edu.br>
2024-12-17 20:08:40 -0300

    Com Codigos para andar e radar

--- Wakanda.java
+++ Wakanda.java
@@ -21,22 +21,33 @@ public class Wakanda extends Robot
 		setColors(Color.black,Color.green,Color.green); // body,gun,radar
 
 		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
-		}
+		while(true){
+    	ahead(100); //Go ahead 100 pixels
+    	turnGunRight(360); //scan
+    	back(75); //Go back 75 pixels
+    	turnGunRight(360); //scan
+
+    //For each second the robot go ahead 25 pixels.
+}
+		
 	}
 
 	/**
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
-	}
+	double distance = e.getDistance(); //get the distance of the scanned robot
+    if(distance > 800) //this conditions adjust the fire force according the distance of the scanned robot.
+        fire(5);
+    else if(distance > 600 && distance <= 800)
+        fire(4);
+    else if(distance > 400 && distance <= 600)
+        fire(3);
+    else if(distance > 200 && distance <= 400)
+        fire(2);
+    else if(distance < 200)
+        fire(1);
+}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
@@ -50,7 +61,8 @@ public class Wakanda extends Robot
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
+		double bearing = e.getBearing(); //get the bearing of the wall
+    	turnRight(-bearing); //This isn't accurate but release your robot.
+    	ahead(100); 
 	}	
 }
