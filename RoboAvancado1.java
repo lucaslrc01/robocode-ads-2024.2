94cc77a77e0f16b3f9fa717b08d5e0baa9759e25
lucas.lc <lucas.lc16@aluno.ifsc.edu.br>
2024-12-17 17:53:57 -0300

    Método para evitar paredes.

--- RoboAvancado1.java
+++ RoboAvancado1.java
@@ -36,6 +36,20 @@ public class RoboAvancado1 extends AdvancedRobot {
         }
 
         setTurnRight(angleToEnemy + 90); 
+		evitarParede();
         setAhead(150); 
+    }
+	
+	private void evitarParede() {
+        double paredeMargem = 50; 
+        double battlefieldWidth = getBattleFieldWidth();
+        double battlefieldHeight = getBattleFieldHeight();
+        double x = getX();
+        double y = getY();
+
+        if (x < paredeMargem || x > battlefieldWidth - paredeMargem || y < paredeMargem || y > battlefieldHeight - paredeMargem) {
+            setTurnRight(90); 
+            setAhead(100); 
+        }
     }
 }
